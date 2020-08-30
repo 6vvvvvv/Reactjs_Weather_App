d7b5fb36602db8114c53738207fa135154e11e26
Lau Wai <lawrenceliuwei@gmail.com>
2020-08-30 03:02:48 +0200

    version29aug2020

--- weatherfrontend/src/App.js
+++ weatherfrontend/src/App.js
@@ -1,35 +1,52 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import { weathericon } from "./config/weathericon";
+import GithubCorner from "react-github-corner";
+import cities from "cities.json";
 
-var clearIcons = 800;
-var cloudIcons = [801, 802, 803, 804];
-var thunderstormIcons = [200, 201, 202, 210, 211, 212, 221, 230, 231, 232];
-var sprinkleIcons = [300, 301, 302, 310, 311, 312, 313, 314, 321];
-var rainIcons = [500, 501, 502, 503, 504, 511, 520, 521, 522, 531];
-var snowIcons = [600, 601, 602, 611, 612, 615, 616, 620, 621, 622];
-var atmosphereIcons = [701, 711, 721, 731, 741, 751, 761, 762, 771, 781];
+// cities=[
+//   {
+//     "country": "FR",
+//     "name": "Lyon",
+//     "lat": "45.75",
+//     "lng": "4.583333"
+//   },
+//   ...
+// ]
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      location: "",
-      countrycode: "",
-      latitude: 0,
-      longitude: 0,
-      fahrenheit: 0,
-      celsius: 0,
-      humidity: 0,
-      windspeed: 0,
-      weatherid: 0
-    };
-  }
+// <Tooltip title="Optional: Enter a two-letter country code after the city name to make the search more precise. For example, London, GB.">
+//                     <Search />
+//                   </Tooltip>
+//                 </InputAdornme
 
-  componentDidMount() {
+const App = () => {
+  const [location, setLocation] = useState("");
+  const [countrycode, setCountrycode] = useState("");
+
+  // const [latitude, setLatitude] = useState(0);
+
+  // const [longitude, setLongitude] = useState(0);
+
+  // const [fahrenheit, setFahrenheit] = useState(0);
+
+  const [celsius, setCelsius] = useState(0);
+
+  const [humidity, setHumidity] = useState(0);
+
+  const [windspeed, setWindspeed] = useState(0);
+
+  const [weatherid, setWeatherid] = useState(0);
+  const [inputvalue, setInputvalue] = useState("");
+
+  const inputsearch = (e) => {
+    setInputvalue(e.target.value);
+  };
+
+  useEffect(() => {
     try {
       fetch("https://ipapi.co/json/")
-        .then(response => response.json())
-        .then(data => {
+        .then((response) => response.json())
+        .then((data) => {
           console.log(data);
 
           fetch(
@@ -39,113 +56,177 @@ class App extends Component {
               data.longitude +
               "&APPID=34e35ced0edac102f995450c1b6d4bae"
           )
-            .then(response1 => response1.json())
-            .then(data1 => {
+            .then((response1) => response1.json())
+            .then((data1) => {
               console.log(data1);
               console.log(data1.weather[0].id);
 
-              this.setState({
-                location: data.city,
-                countrycode: data.country_code,
-                latitude: data.latitude,
-                longitude: data.longitude,
-                fahrenheit: ((data1.main.temp - 273.15) * 1.8 + 32).toFixed(1),
-                celsius: (data1.main.temp - 273.15).toFixed(1),
-                humidity: data1.main.humidity.toFixed(1),
-                windspeed: (data1.wind.speed * 3.6).toFixed(1),
-                weatherid: data1.weather[0].id
-              });
+              setLocation(data.city);
+              setCountrycode(data.country_code);
+              // setLatitude(data.latitude);
+              // setLongitude(data.longitude);
+              // setFahrenheit(((data1.main.temp - 273.15) * 1.8 + 32).toFixed(1));
+              setCelsius((data1.main.temp - 273.15).toFixed(1));
+              setHumidity(data1.main.humidity.toFixed(1));
+              setWindspeed((data1.wind.speed * 3.6).toFixed(1));
+              setWeatherid(data1.weather[0].id);
             });
         });
-    } catch (e) {
-      console.log(e);
+    } catch (err) {
+      console.log(err);
     }
-  }
+  }, []);
 
-  weather = wid => {
-    if (wid === clearIcons) {
+  const weather = (wid) => {
+    if (wid === weathericon.clearIcons) {
       return <i className="wi wi-day-sunny"></i>;
     }
 
-    for (let i = 0; i < cloudIcons.length; i++) {
-      if (wid == cloudIcons[i]) {
+    for (let i = 0; i < weathericon.cloudIcons.length; i++) {
+      if (wid === weathericon.cloudIcons[i]) {
         return <i className="wi wi-cloudy"></i>;
       }
     }
 
-    for (let i = 0; i < thunderstormIcons.length; i++) {
-      if (wid == thunderstormIcons[i]) {
+    for (let i = 0; i < weathericon.thunderstormIcons.length; i++) {
+      if (wid === weathericon.thunderstormIcons[i]) {
         return <i className="wi wi-thunderstorm"></i>;
       }
     }
 
-    for (let i = 0; i < sprinkleIcons.length; i++) {
-      if (wid == sprinkleIcons[i]) {
+    for (let i = 0; i < weathericon.sprinkleIcons.length; i++) {
+      if (wid === weathericon.sprinkleIcons[i]) {
         return <i className="wi wi-sprinkle"></i>;
       }
     }
 
-    for (let i = 0; i < rainIcons.length; i++) {
-      if (wid == rainIcons[i]) {
+    for (let i = 0; i < weathericon.rainIcons.length; i++) {
+      if (wid === weathericon.rainIcons[i]) {
         return <i className="wi wi-rain"></i>;
       }
     }
 
-    for (let i = 0; i < snowIcons.length; i++) {
-      if (wid == snowIcons[i]) {
+    for (let i = 0; i < weathericon.snowIcons.length; i++) {
+      if (wid === weathericon.snowIcons[i]) {
         return <i className="wi wi-snow"></i>;
       }
     }
 
-    for (let i = 0; i < atmosphereIcons.length; i++) {
-      if (wid == atmosphereIcons[i]) {
+    for (let i = 0; i < weathericon.atmosphereIcons.length; i++) {
+      if (wid === weathericon.atmosphereIcons[i]) {
         return <i className="wi wi-alien"></i>;
       }
     }
   };
 
-  render() {
-    return (
-      <div>
-        <div className="container">
-          <div className="contHead">
+  return (
+    <div>
+      <div className="container">
+        <div className="searchbar">
+          <i className="fas fa-search-location searchicon "></i>
+          <input
+            className="searchinput"
+            type="text"
+            value={inputvalue}
+            onChange={inputsearch}
+          />
+          <a className="btn-floating btn-medium pulse floating-pulse">
+            <i className="material-icons">cloud</i>
+          </a>
+        </div>
+
+        <div className="col s12">
+          <div className="place col s6">
             <p>
-              {this.state.location} , {this.state.countrycode}{" "}
-              {this.weather(this.state.weatherid)}
+              {location} , {countrycode}
+            </p>
+            <p>
+              <i className="wi wi-thermometer"></i> {celsius}{" "}
+              <i className="wi wi-celsius"></i>
             </p>
           </div>
-
-          <hr></hr>
-
-          <div className="contContent">
-            <div>
-              <p>
-                <i className="wi wi-thermometer"></i> {this.state.celsius}{" "}
-                <i className="wi wi-celsius"></i>
-              </p>
-            </div>
+          <div className="weathericon col s6">{weather(weatherid)}</div>
+        </div>
+        {/* 
+        <div className="contContent">
+          <div>
+            <p>
+              <i className="wi wi-thermometer"></i> {celsius}{" "}
+              <i className="wi wi-celsius"></i>
+            </p>
           </div>
+        </div> */}
 
-          <div className="bottombox">
-            <div id="bottombox">
-              <div className="bottomleft">
-                <span>
-                  <i className="wi wi-humidity"></i>
-                </span>
-                {this.state.humidity}%
+        <div className="row">
+          <div className="col s1">
+            <span>
+              <i className="wi wi-humidity"></i>
+              <p>{humidity}%</p>
+            </span>
+          </div>
+          <div className="col s1 offset-s2">
+            <span>
+              <i className="wi wi-strong-wind"></i>
+              <p>{windspeed}km/h</p>
+            </span>
+          </div>
+        </div>
+
+        {/* <hr></hr> */}
+        <div class="card">
+          <div class="card-tabs  ">
+            <ul class="tabs tabs-fixed-width ">
+              <li class="tab">
+                <a class="active" href="#n4d">
+                  Next 4 Days
+                </a>
+              </li>
+              <li class="tab">
+                <a href="#trend">Trend Curve</a>
+              </li>
+            </ul>
+          </div>
+          <div class="card-content teal lighten-3">
+            <div id="n4d">
+              <div>
+                <div className="row">
+                  <p>Stuff</p>
+                </div>
+                <div className="row">
+                  <p>Stuff</p>
+                </div>
+                <div className="row">
+                  <p>Stuff</p>
+                </div>
+                <div className="row">
+                  <p>Stuff</p>
+                </div>
               </div>
-              <div className="bottomright">
-                <span>
-                  <i className="wi wi-strong-wind"></i>
-                </span>
-                {this.state.windspeed}km/h
-              </div>
+              <div id="trend"></div>
             </div>
           </div>
         </div>
+        <div className="footer-copyright footer ">
+          © 2020 Copyright
+          <span className="right">
+            <i class="fab fa-github"></i>
+            <a
+              className="white-text text-lighten-1 github "
+              href="https://github.com/6vvvvvv/Reactjs_Weather_App"
+            >
+              Lau's Github
+            </a>
+          </span>
+        </div>
+        <GithubCorner
+          href="https://github.com/6vvvvvv/Reactjs_Weather_App"
+          bannerColor="#bfdcae"
+          size={120}
+          direction="right"
+        />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
